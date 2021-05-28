9d41ae3b5d860ff47a11b5117f894fab8a837762
Taiwo Lawal <taiwolawal360@gmail.com>
2021-05-28 10:23:24 +0100

    - Initial Project

---
+++ app/src/main/java/com/example/android/nigeriaweatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.nigeriaweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
