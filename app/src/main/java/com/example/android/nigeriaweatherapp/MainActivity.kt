b140974c23dc48426adcf10629fa3f6e0c998a0e
Taiwo Lawal <taiwolawal360@gmail.com>
2021-05-28 12:14:58 +0100

    - List of states in Nigeria added via spinner and included button to search weather forecast.

--- app/src/main/java/com/example/android/nigeriaweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/android/nigeriaweatherapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.android.nigeriaweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.android.nigeriaweatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var stateSpinner: Spinner
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        stateSpinner = binding.stateSpinner
+        ArrayAdapter.createFromResource(this, R.array.states_list_filter, android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                stateSpinner.adapter = adapter
+            }
+
     }
 }
